8e8631cd517f7a9d014029045f8e509438bb0065
0xbcdev <0xbcdev@bcdev.tools>
2024-05-03 22:08:04 +0700

    try wrap tx

--- be_rpc/backend/transactions.go
+++ be_rpc/backend/transactions.go
@@ -11,6 +11,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/client"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/tx"
+	tx2 "github.com/cosmos/cosmos-sdk/x/auth/tx"
 	authztypes "github.com/cosmos/cosmos-sdk/x/authz"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	crisistypes "github.com/cosmos/cosmos-sdk/x/crisis/types"
@@ -126,7 +127,7 @@ func (m *Backend) getTransactionsInBlock(height int64) (blockInfo map[string]any
 		var tmTx, tmTx2 tmtypes.Tx
 
 		tx := resBlock.Txs[txIdx]
-		tmTx, err = m.clientCtx.TxConfig.TxEncoder()(tx)
+		tmTx, err = m.clientCtx.TxConfig.TxEncoder()(tx2.WrapTx(tx).(sdk.Tx))
 		if err != nil {
 			err = errors.Wrap(err, "failed to encode tx to tm tx")
 			return
